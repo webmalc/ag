048cb653a6dfe75714478e7e5c474f03bb063da3
Dubovskiy Sergey <webmalc@gmail.com>
2014-03-03 19:00:19 +0400

    AngularJs autofill fix

--- src/AG/Bundle/BaseBundle/Resources/public/js/app/001-app.js
+++ src/AG/Bundle/BaseBundle/Resources/public/js/app/001-app.js
@@ -3,6 +3,7 @@ var navbarApp = angular.module('navbarApp', []);
 angular.module('agApp', [
     'ui.bootstrap',
     'ag.directives.dropdown',
+    'ag.directives.formAutofillFix',
     'navbarApp'
 ]).config(['$httpProvider', '$interpolateProvider', function ($httpProvider, $interpolateProvider) {
     'use strict';
