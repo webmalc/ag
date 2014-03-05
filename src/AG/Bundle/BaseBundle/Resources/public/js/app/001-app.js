ca05db9f40f7eba243bcc75e0a579e6eaedc6364
Dubovskiy Sergey <webmalc@gmail.com>
2014-03-05 13:06:56 +0400

    Add login page

--- src/AG/Bundle/BaseBundle/Resources/public/js/app/001-app.js
+++ src/AG/Bundle/BaseBundle/Resources/public/js/app/001-app.js
@@ -3,7 +3,6 @@ var navbarApp = angular.module('navbarApp', []);
 angular.module('agApp', [
     'ui.bootstrap',
     'ag.directives.dropdown',
-    'ag.directives.formAutofillFix',
     'navbarApp'
 ]).config(['$httpProvider', '$interpolateProvider', function ($httpProvider, $interpolateProvider) {
     'use strict';
